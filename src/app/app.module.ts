55f37e25a29990086205b684fa8ddc7e2f641770
christianarredondo <christianarredondo@utexas.edu>
2017-07-16 15:00:11 -0500

    Set up initial MD menu and grid using dummy data from JSONplaceholder.

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MdButtonModule,
+  MdCardModule,
+  MdMenuModule,
+  MdToolbarModule,
+  MdIconModule,
+  MdGridListModule,
+  MdListModule } from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    MdButtonModule,
+    MdCardModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdGridListModule,
+    MdListModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
